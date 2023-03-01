857b313fbb075598faee9d2af060605147b2265b
D3Portillo <d3portillo@gmail.com>
2023-03-01 22:01:35 +0100

    feat(pages/index.tsx): add react-icons example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import SeoTags from "@/components/SeoTags"
+import { GiIncomingRocket } from "react-icons/gi"
+// @see http://react-icons.github.io/react-icons/search?q=rock
 
 export default function Home() {
   return (
     <>
       <SeoTags />
-      <main className="p-4 font-semibold text-5xl">NextJS 13</main>
+      <main className="p-4">
+        <GiIncomingRocket className="text-[calc(5rem+5vw)] text-some-color" />
+        <span className="font-semibold text-3xl lg:text-5xl block pt-4">
+          NextJS 13
+        </span>
+      </main>
     </>
   )
 }
